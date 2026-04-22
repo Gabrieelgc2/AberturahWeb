c77f8dc8f6de3fc6e5e32fce760b680a05be6640
Gabrieelgc2 <gabrielao8@hotmail.com>
2026-04-22 10:06:25 -0300

    add: main.tsx styles.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
+import "./styles.css"
 
 const router = getRouter();
 
